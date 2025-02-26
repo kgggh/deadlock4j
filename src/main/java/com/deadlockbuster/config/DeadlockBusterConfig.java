1557badbba41890147596e4e36f73306dfde272e
kgggh <68227318+kgggh@users.noreply.github.com>
2025-02-26 20:54:11 +0900

    feat: config 추가

---
+++ src/main/java/com/deadlockbuster/config/DeadlockBusterConfig.java
@@ -0,0 +1,79 @@
+package com.deadlockbuster.config;
+
+import java.util.Properties;
+
+public class DeadlockBusterConfig {
+    private boolean enable;
+    private boolean logEnable;
+    private boolean sendEvent;
+    private String databaseType;
+
+    private String centralServerIp;
+    private int centralServerPort;
+
+    public DeadlockBusterConfig() {
+        this.enable = true;
+        this.logEnable = true;
+        this.sendEvent = false;
+        this.databaseType = null;
+        this.centralServerIp = null;
+        this.centralServerPort = 8282;
+    }
+
+    private void applyProperties(Properties properties) {
+        this.enable = Boolean.parseBoolean(properties.getProperty("deadlock-buster.enable", String.valueOf(enable)));
+        this.logEnable = Boolean.parseBoolean(properties.getProperty("deadlock-buster.log-enable", String.valueOf(logEnable)));
+        this.sendEvent = Boolean.parseBoolean(properties.getProperty("deadlock-buster.send-event", String.valueOf(sendEvent)));
+        this.databaseType = properties.getProperty("deadlock-buster.database-type", databaseType);
+        this.centralServerIp = properties.getProperty("deadlock-buster.central-server-ip", centralServerIp);
+        this.centralServerPort = Integer.parseInt(properties.getProperty("deadlock-buster.central-server-port", String.valueOf(centralServerPort)));
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public boolean isLogEnable() {
+        return logEnable;
+    }
+
+    public void setLogEnable(boolean logEnable) {
+        this.logEnable = logEnable;
+    }
+
+    public boolean isSendEvent() {
+        return sendEvent;
+    }
+
+    public void setSendEvent(boolean sendEvent) {
+        this.sendEvent = sendEvent;
+    }
+
+    public String getDatabaseType() {
+        return databaseType;
+    }
+
+    public void setDatabaseType(String databaseType) {
+        this.databaseType = databaseType;
+    }
+
+    public String getCentralServerIp() {
+        return centralServerIp;
+    }
+
+    public void setCentralServerIp(String centralServerIp) {
+        this.centralServerIp = centralServerIp;
+    }
+
+    public int getCentralServerPort() {
+        return centralServerPort;
+    }
+
+    public void setCentralServerPort(int centralServerPort) {
+        this.centralServerPort = centralServerPort;
+    }
+}
